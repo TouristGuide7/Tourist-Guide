fbe11a12a077ae78ba265ccd22b65956a3287e85
Sameer Khan <sameerkhansk1051@gmail.com>
2023-07-13 01:24:52 +0500

    tourist-guide

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const gName = document.getElementById("getName")
+const gCity = document.getElementById("getCity")
+const names = document.getElementById("name")
+const city = document.getElementById("city")
+
+
+const btns = document.getElementById("checks").addEventListener("click", () =>{
+  localStorage.setItem("UserName" , gName.value)
+  localStorage.setItem("UserCity" , gCity.value)
+  let localName = localStorage.getItem("userName")
+let localCity = localStorage.getItem("userCity")
+console.log(localName);
+console.log(localCity);
+})
+
+
+
 const seasons = document.querySelector("#seasons");
 const btn = document.getElementById("submit").addEventListener("click", () => {
   const seasonsElement = seasons.value;
