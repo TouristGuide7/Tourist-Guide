19d46e238f9a6dde0f8642cb1162dde816550c17
Sameer Khan <sameerkhansk1051@gmail.com>
2023-07-21 18:36:19 +0500

    tourist-guide

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-const gName = document.getElementById("getName")
-const gCity = document.getElementById("getCity")
-const names = document.getElementById("name")
-const city = document.getElementById("city")
-
-
-const btns = document.getElementById("checks").addEventListener("click", () =>{
-  localStorage.setItem("UserName" , gName.value)
-  localStorage.setItem("UserCity" , gCity.value)
+function user(){
+  const gName = document.getElementById("userName")
+  const gCity = document.getElementById("userCity")
   let localName = localStorage.getItem("userName")
-let localCity = localStorage.getItem("userCity")
-console.log(localName);
-console.log(localCity);
-})
+  let localCity = localStorage.getItem("cityName")
+  gName.innerHTML += localName
+  gCity.innerHTML += localCity
+}
+
+user()
 
 
 
-const seasons = document.querySelector("#seasons");
-const btn = document.getElementById("submit").addEventListener("click", () => {
-  const seasonsElement = seasons.value;
-  if (seasonsElement === "Summer") {
-    document.getElementById("btnlink").href = "Tourist_Summer.html";
-  } else if (seasonsElement === "Winter") {
-    document.getElementById("btnlink").href = "Tourist_Winter.html";
-  } else if (seasonsElement === "Autumn") {
-    document.getElementById("btnlink").href = "Tourist_Automn.html";
-  } else {
-    document.getElementById("btnlink").href = "Tourist_Spring.html";
-  }
-});
+
+
