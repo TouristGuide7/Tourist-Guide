aa8b08d2f55c46a3fab2baa6faebb41a092a64ca
Sameer Khan <sameerkhansk1051@gmail.com>
2023-07-27 23:13:41 +0500

    touristguide

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ function user(){
   gName.innerHTML += localName
   gCity.innerHTML += localCity
 }
-
 user()
+const travelCity = document.querySelectorAll(".localCity")
+const countryDiscription = document.querySelectorAll(".description")
+const country = document.querySelectorAll(".country1")
+const countryCulture = document.querySelectorAll(".culture")
+const famousCountry = document.querySelectorAll(".fCountry")
 
 
 const touristAPI = "data.json"
 async function newFunction(){
   const response = await fetch(touristAPI)
   const data = await response.json()
-
-  console.log(data);
+  for(let i = 0 ; i < travelCity.length; i++){
+    travelCity[i].innerHTML = data.world[i].place
+    countryDiscription[i].innerHTML = data.world[i].smallDiscription
+    country[i].innerHTML += data.world[i].country
+    countryCulture[i].innerHTML += data.world[i].culture
+    famousCountry[i].innerHTML = data.famousPlace[i].place
+  }
 }
 
 newFunction()
