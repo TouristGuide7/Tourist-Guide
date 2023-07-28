d15283bba536253c59df299038b4b02d789518e0
Sameer Khan <sameerkhansk1051@gmail.com>
2023-07-28 20:59:16 +0500

    touristguide

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const country = document.querySelectorAll(".country1")
 const countryCulture = document.querySelectorAll(".culture")
 const famousCountry = document.querySelectorAll(".fCountry")
 const mainCountry = document.querySelectorAll(".mainCountry")
-console.log(mainCountry);
+const cardCountry = document.querySelectorAll(".Country")
+const cardDesc = document.querySelectorAll(".page-matter")
 
 
 const touristAPI = "data.json"
@@ -21,14 +22,18 @@ async function newFunction(){
   const response = await fetch(touristAPI)
   const data = await response.json()
   for(let i = 0 ; i < travelCity.length; i++){
-    travelCity[i].innerHTML = data.world[i].place
+    travelCity[i].innerHTML = data.world[i].country
     countryDiscription[i].innerHTML = data.world[i].smallDiscription
     country[i].innerHTML += data.world[i].country
     countryCulture[i].innerHTML += data.world[i].culture
     famousCountry[i].innerHTML = data.famousPlace[i].mplace
   }
   for(let j = 0; j < mainCountry.length; j++){
-    mainCountry[j].innerHTML = data.world[j].place
+    mainCountry[j].innerHTML = data.world[j].country
+  }
+  for(let i = 0; i < cardCountry.length; i++){
+    cardCountry[i].innerHTML = data.world[i].country
+    cardDesc[i].innerHTML = data.world[i].description
 
   }
 }
