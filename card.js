4b0df345455c5c7b54381712a5678bd154364c3d
Sameer Khan <sameerkhansk1051@gmail.com>
2023-08-20 00:17:27 +0500

    dkjhdfdsa

--- card.js
+++ card.js
@@ -3,22 +3,51 @@ function user() {
   const gCity = document.getElementById("userCity");
   let localName = localStorage.getItem("userName");
   let localCity = localStorage.getItem("cityName");
+
   gName.innerHTML += localName;
   gCity.innerHTML += localCity;
 }
 user();
 let getNumValue = localStorage.getItem("numValue");
+let getPlaceValue = localStorage.getItem("placeValue");
+console.log(getPlaceValue);
 
 const countryDiscription = document.getElementById("description");
 const country = document.getElementById("country");
+const mainCardImage = document.getElementById("mainCardImg")
+
 const touristAPI = "data.json";
 async function newFunction() {
   const response = await fetch(touristAPI);
   const data = await response.json();
-  function card(a) {
-    countryDiscription.innerHTML = data.winter[a].description;
-    country.innerHTML = data.winter[a].country;
+  function card(num, place) {
+    if(place == "winter"){
+      countryDiscription.innerHTML = data.winter[num].description;
+      country.innerHTML = data.winter[num].country;
+      mainCardImage.src = data.winter[num].imgUrl
+    }
+    else if (place == "summer"){
+      countryDiscription.innerHTML = data.summer[num].description;
+      country.innerHTML = data.summer[num].country;
+      mainCardImage.src = data.summer[num].imgUrl
+    }
+    else if (place == "spring"){
+      countryDiscription.innerHTML = data.spring[num].description;
+      country.innerHTML = data.spring[num].country;
+      mainCardImage.src = data.spring[num].imgUrl
+    }
+    else if (place == "autumn"){
+      countryDiscription.innerHTML = data.autumn[num].description;
+      country.innerHTML = data.autumn[num].country;
+      mainCardImage.src = data.autumn[num].imgUrl
+    }
+    else if (place == "famous_places"){
+      countryDiscription.innerHTML = data.famous_places[num].description;
+      country.innerHTML = data.famous_places[num].country;
+      mainCardImage.src = data.famous_places[num].imgUrl
+    }
+
   }
-  card(getNumValue);
+  card(getNumValue , getPlaceValue);
 }
 newFunction();
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function buttonRender(num){
+function buttonRender(num, place){
   localStorage.setItem("numValue" , num)
+  localStorage.setItem("placeValue" , place)
 }
 
 
@@ -19,20 +20,16 @@ const countryDiscription = document.querySelectorAll(".description")
 const continent = document.querySelectorAll(".country1")
 const countryCulture = document.querySelectorAll(".culture")
 const countryImage = document.querySelectorAll(".countryImage")
-const cardImage = document.querySelectorAll(".winterImage")
-
-
-
 const famousCountry = document.querySelectorAll(".fCountry")
+const famousCountryImg = document.querySelectorAll(".fImg")
 const mainCountry = document.querySelectorAll(".mainCountry")
 const totalCardLength = document.querySelectorAll(".cardItem")
-
-
-
 const touristAPI = "data.json"
 async function newFunction(){
   const response = await fetch(touristAPI)
   const data = await response.json()
+
+
   for(let i = 0 ; i < totalCardLength.length; i++){
     travelCity[i].innerHTML = data.winter[i].country
     countryDiscription[i].innerHTML = data.winter[i].smallDiscription
@@ -40,12 +37,9 @@ async function newFunction(){
     countryCulture[i].innerHTML += `: ${data.winter[i].culture}`
     countryImage[i].src = data.winter[i].imgUrl
   }
-  // for(let j = 0; j < mainCountry.length; j++){
-  //   mainCountry[j].innerHTML = data.world[j].winter[i].country
-  // }
-  // for(let i= 0; i < famousCountry.length; i++){
-  //   famousCountry[i].innerHTML = data.famousPlace[i].place
-  // }
+  for(let i= 0; i < famousCountry.length; i++){
+    famousCountry[i].innerHTML = data.famous_places[i].country
+  }
 
 }
 
