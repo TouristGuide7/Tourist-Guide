eb821c06b4027a8c7c0b67acf0470fd57d547183
Sameer Khan <sameerkhansk1051@gmail.com>
2023-08-20 12:35:12 +0500

    change all

--- travel.js
+++ travel.js
@@ -10,19 +10,39 @@ function user(){
   const travelCity = document.querySelectorAll(".localCity")
 const countryImage = document.querySelectorAll(".countryImage")
 
+const mainCardBtnValue = document.getElementById("cardMainButton")
 
+console.log(mainCardBtnValue.value);
 
 const touristAPI = "data.json"
 async function newFunction(){
   const response = await fetch(touristAPI)
   const data = await response.json()
-  console.log(data.winter[1].country);
 
+if(mainCardBtnValue.value = "winter"){
   for(let i = 0 ; i < travelCity.length; i++){
     travelCity[i].innerHTML = data.winter[i].country
     countryImage[i].src = data.winter[i].imgUrl
   }
-
+}
+else if(mainCardBtnValue.value = "autumn"){
+  for(let i = 0 ; i < travelCity.length; i++){
+    travelCity[i].innerHTML = data.autumn[i].country
+    countryImage[i].src = data.autumn[i].imgUrl
+  }
+}
+else if(mainCardBtnValue.value = "spring"){
+  for(let i = 0 ; i < travelCity.length; i++){
+    travelCity[i].innerHTML = data.spring[i].country
+    countryImage[i].src = data.spring[i].imgUrl
+  }
+}
+else if(mainCardBtnValue.value = "summer"){
+  for(let i = 0 ; i < travelCity.length; i++){
+    travelCity[i].innerHTML = data.summer[i].country
+    countryImage[i].src = data.summer[i].imgUrl
+  }
+}
 }
 
 newFunction()
